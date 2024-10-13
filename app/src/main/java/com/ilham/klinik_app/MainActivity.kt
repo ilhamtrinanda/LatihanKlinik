dab83841822e4e839c7cb849d7cac655ed57ae63
ilhamtrinanda <ilhmtrnnda@gmail.com>
2024-10-13 18:50:22 +0700

    update

--- app/src/main/java/com/ilham/klinik_app/MainActivity.kt
+++ app/src/main/java/com/ilham/klinik_app/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.ilham.klinik_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +14,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)  
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                // ini setelah delay akan pindah ke page berikutnya secara otomatis
+                val intent = Intent(this, StarterPageActivity::class.java)
+                startActivity(intent)
+                finish()// agar ketika sudah pindah ke page berikutnya, ini page nya ga back
+            }, 3000
+        )
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
